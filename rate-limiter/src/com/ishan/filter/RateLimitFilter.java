b84d973f647c49f21712b5a20d16a618aaaefffd
Ishan <ishanjain1991@gmail.com>
2018-03-21 15:27:10 +0530

    adding redis jar

--- rate-limiter/src/com/ishan/filter/RateLimitFilter.java
+++ rate-limiter/src/com/ishan/filter/RateLimitFilter.java
@@ -1,6 +1,9 @@
 package com.ishan.filter;
 
+import com.ishan.base.HttpMethod;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -12,6 +15,11 @@ public class RateLimitFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String requestURI = httpServletRequest.getRequestURI();
+        String method = httpServletRequest.getMethod();
+        HttpMethod httpMethod = HttpMethod.valueOf(method);
 
+        chain.doFilter(request, response);
     }
 }
