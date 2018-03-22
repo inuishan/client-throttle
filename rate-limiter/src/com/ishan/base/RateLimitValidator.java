3fabebf44f8ab91b49fcf2a7a08bb6968097fc16
Ishan <ishanjain1991@gmail.com>
2018-03-22 11:22:13 +0530

    added static constructor methods

--- rate-limiter/src/com/ishan/base/RateLimitValidator.java
+++ rate-limiter/src/com/ishan/base/RateLimitValidator.java
@@ -20,9 +20,62 @@ public class RateLimitValidator {
      * Holder class for the response
      */
     public static class RateLimitResponse {
-
+        /**
+         * Whether the rate limit was reached or not
+         */
         private boolean rateLimitReached;
 
+        /**
+         * If the rate limit was reached, then which period's limit that it violated
+         */
+        private RateLimitPeriod rateLimitPeriod;
 
+        /**
+         * If the rate limit was reached, then which limit did the request break
+         */
+        private RateLimitViolationCause rateLimitViolationCause;
+
+        //Enforcing use of static constructor
+        private RateLimitResponse() {
+
+        }
+
+        public static RateLimitResponse withRateLimitReached(RateLimitPeriod period, RateLimitViolationCause cause) {
+            RateLimitResponse rateLimitResponse = new RateLimitResponse();
+            rateLimitResponse.setRateLimitReached(true);
+            rateLimitResponse.setRateLimitPeriod(period);
+            rateLimitResponse.setRateLimitViolationCause(cause);
+            return rateLimitResponse;
+        }
+
+        public static RateLimitResponse withRateLimitNotReached() {
+            RateLimitResponse rateLimitResponse = new RateLimitResponse();
+            rateLimitResponse.setRateLimitReached(false);
+            return rateLimitResponse;
+        }
+
+        public boolean getRateLimitReached() {
+            return rateLimitReached;
+        }
+
+        public void setRateLimitReached(boolean rateLimitReached) {
+            this.rateLimitReached = rateLimitReached;
+        }
+
+        public RateLimitPeriod getRateLimitPeriod() {
+            return rateLimitPeriod;
+        }
+
+        public void setRateLimitPeriod(RateLimitPeriod rateLimitPeriod) {
+            this.rateLimitPeriod = rateLimitPeriod;
+        }
+
+        public RateLimitViolationCause getRateLimitViolationCause() {
+            return rateLimitViolationCause;
+        }
+
+        public void setRateLimitViolationCause(RateLimitViolationCause rateLimitViolationCause) {
+            this.rateLimitViolationCause = rateLimitViolationCause;
+        }
     }
 }
