9fd4810442005fb536791fad370cbefec8fc1ca9
Ishan <ishanjain1991@gmail.com>
2018-03-22 12:14:18 +0530

    added wrap function

--- rate-limiter/src/com/ishan/base/RateLimitPeriod.java
+++ rate-limiter/src/com/ishan/base/RateLimitPeriod.java
@@ -1,12 +1,14 @@
 package com.ishan.base;
 
+import org.joda.time.DateTime;
+
 /**
  * @author ishanjain
  * @since 22/03/18
  */
 public enum RateLimitPeriod {
 
-    SECOND, MINUTE, HOUR, WEEK, MONTH;
+    SECOND, MINUTE, HOUR, DAY, WEEK, MONTH;
 
     /**
      * This wraps the time to the nearest floored value, 1.2 seconds will be 1
@@ -15,6 +17,20 @@ public enum RateLimitPeriod {
      * @return The wrapped time
      */
     public long wrap(long time) {
-        new DateTime()
+        DateTime dateTime = new DateTime(time);
+        switch (this) {
+            case SECOND:
+                return dateTime.withMillisOfSecond(0).getMillis();
+            case MINUTE:
+                return dateTime.withSecondOfMinute(0).withMillisOfSecond(0).getMillis();
+            case HOUR:
+                return dateTime.withMinuteOfHour(0).withSecondOfMinute(0).withMillisOfSecond(0).getMillis();
+            case DAY:
+                return dateTime.millisOfDay().withMinimumValue().getMillis();
+            case WEEK:
+                return dateTime.dayOfWeek().withMinimumValue().millisOfDay().withMinimumValue().getMillis();
+            case MONTH:
+                return dateTime.dayOfMonth().withMinimumValue().millisOfDay().withMinimumValue().getMillis();
+        }
     }
 }
